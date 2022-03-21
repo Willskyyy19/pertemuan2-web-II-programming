b2ed17c503a7655a70633e8604075d4f907fde14
william diar dwiaji <102008444+Willskyyy19@users.noreply.github.com>
2022-03-21 20:20:07 +0700

    Add files via upload

---
+++ Contoh_1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "Perkenalkan";
+ echo"Nama saya William
+ Saya tinggal di daerah Depok
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
